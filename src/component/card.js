4c70a4019a17175ddf91cb18852b6837a9a42164
Depurk <2depurk@gmail.com>
2021-12-01 01:45:40 +0530

    added bootstrap

---
+++ src/component/card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+
+const card = () => {
+    return (
+        <>
+            <Card className="d-flex justify-content-center" style={{ width: '18rem', }}>
+                <Card.Img variant="top" src="https://static.remove.bg/remove-bg-web/a76316286d09b12be1ebda3b400e3f44716c24d0/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg?text=Image cap" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default card
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/navbar'
+import Card from '../component/card'
+
+const index = () => {
+    return (
+        <>
+           <Navbar/>
+           <Card/> 
+        </>
+    )
+}
+
+export default index
