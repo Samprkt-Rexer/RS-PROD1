96dab683bf3e942ef4a5611e96766208f847d658
Shubham Raj <shubham14p3@gmail.com>
2021-12-02 01:06:48 +0530

    adding clone to setupData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Index from './pages/index'
+// import Index from './pages/index'
+import React, { useState,useEffect } from 'react';
 
 function App() {
-  return (
-    
-    <Index />
-    
-  );
-}
+  const [count, setCount] = useState(0);
+   
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+    console.log({count});
+  });
+
+    return (
+       <div>
+      <p>You clicked {count} times</p>
+      {console.log({count})}
+         <button onClick={() => setCount(count + 1)}>
+       Click me
+        </button>
+    </div>
+    );
+  }
+
+ 
+
 
 export default App;
